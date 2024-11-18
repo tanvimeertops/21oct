7c1c94d83eec81062e2c6af92b695cf516611a7a
tanvimeertops <tanvimeer.tops@gmail.com>
2024-11-18 09:02:13 +0530

    18 nov

--- src/com/basics/PatternDemo1.java
+++ src/com/basics/PatternDemo1.java
@@ -1,5 +1,6 @@
 package com.basics;
 /*
+ * 
  * 1
  * 12
  * 123
@@ -53,13 +54,35 @@ public static void main(String[] args) {
 	 * for k
 	 * completely depends on i
 	 */
-	
-	for (int i = 1; i <=5; i++) { //row
-		for (int k = i; k <=5; k++) {
+	/*
+	 
+	 *      1
+	 *     1 2
+	 *    1 2 3
+	 *   1 2 3 4
+	 *  1 2 3 4 5
+	 		 *     1   1*2  2
+	 		***    3   2*2  4
+	 	   *****   5   3*2  6
+	 	    ***
+	 	     *
+	 */
+	for (int i = 1; i <=3; i++) {
+		for (int k = i; k <=3; k++) {
 			System.out.print(" ");
 		}
-		for (int j = 1; j <=i; j++) { //col
-			System.out.print(j);
+		for (int j = 1; j <=i*2-1; j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+	
+	for (int i = 2; i >=1; i--) {
+		for (int k = i; k <=3; k++) {
+			System.out.print(" ");
+		}
+		for (int j = i*2-1; j >=1; j--) {
+			System.out.print("*");
 		}
 		System.out.println();
 	}
