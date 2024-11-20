a3df49a1ab97a5b3bf3cd12e3e0095237e71c716
tanvimeertops <tanvimeer.tops@gmail.com>
2024-11-20 09:23:47 +0530

    sort

--- src/com/oops/ArrayDemo.java
+++ src/com/oops/ArrayDemo.java
@@ -32,5 +32,22 @@ public static void main(String[] gfgfdg) {
 	for (int i = 0; i < a.length; i++) {
 		System.out.println(a[i]);
 	}
+	
+	//Selection sort
+	
+	for (int i = 0; i < a.length; i++) {
+		for (int j =i+1; j < a.length; j++) {
+			if(a[i]>a[j]) {
+				int temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	
+	System.out.println("after sort");
+	for (int i = 0; i < a.length; i++) {
+		System.out.println(a[i]);
+	}
 }
 }
