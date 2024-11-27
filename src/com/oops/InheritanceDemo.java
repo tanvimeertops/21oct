8d58a6a930d514f4c97a0c280be8be85f6535aed
tanvimeertops <tanvimeer.tops@gmail.com>
2024-11-27 09:25:45 +0530

    inheritance

---
+++ src/com/oops/InheritanceDemo.java
@@ -0,0 +1,23 @@
+package com.oops;
+/*
+ * Inheritance : deriving a attribute from another 
+ * class
+ * -->object will be made for last child only
+ * --> java does not directly support multiple inheritance
+ * --> Use "extends" keyword to inherit a class
+ * adv: 
+ * 1.less object creation
+ * 
+ * types:
+ * Single :one parent one child
+ * Multilevel : one grandparent one parent one child
+ * Multiple: multiple parent single child
+ * Hierarchical :one parent multiple child
+ * Hybrid : combination of any two
+ * 
+ */
+public class InheritanceDemo {
+public static void main(String[] args) {
+	
+}
+}
